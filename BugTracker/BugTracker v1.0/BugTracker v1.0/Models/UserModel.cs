2d5b702cf587f6ae0d3ce902816ecfeb1c852724
Talgattt <n.talgat.93@gmail.com>
2015-03-01 14:34:39 +0600

    models are created

---
+++ BugTracker/BugTracker v1.0/BugTracker v1.0/Models/UserModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker_v1._0.Models
+{
+    class UserModel
+    {
+        private long id;
+        private string name;
+        private string surname;
+        private short status;
+
+        public long Id
+        {
+            get { return id;  }
+            set { id = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+
+        public short Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+
+    }
+}
