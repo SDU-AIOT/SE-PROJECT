a7c1165720fc19ab8ab8b8087f2c81747ace50dd
YergaliyevAdiL <adil_ergaliev@mail.ru>
2015-03-03 16:37:31 +0600

    Server Added

---
+++ BugTracker/BugTracker v1.0/BugTracker v1.0 (Server)/ServerForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace BugTracker_v1._0__Server_
+{
+    partial class ServerForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.clientNumberLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of Clients:";
+            // 
+            // clientNumberLabel
+            // 
+            this.clientNumberLabel.AutoSize = true;
+            this.clientNumberLabel.Location = new System.Drawing.Point(112, 13);
+            this.clientNumberLabel.Name = "clientNumberLabel";
+            this.clientNumberLabel.Size = new System.Drawing.Size(13, 13);
+            this.clientNumberLabel.TabIndex = 1;
+            this.clientNumberLabel.Text = "0";
+            // 
+            // ServerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 262);
+            this.Controls.Add(this.clientNumberLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "ServerForm";
+            this.Text = "Server";
+            this.Load += new System.EventHandler(this.Server_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label clientNumberLabel;
+    }
+}
+
