eb52341fc7d56b4651fa502d4ce10a0f321fb2ac
Isco <islam.mb.94@gmail.com>
2015-03-03 16:56:20 +0600

    Login form was added

--- BugTracker/BugTracker v1.0/BugTracker v1.0/Forms/LoginForm.cs
+++ BugTracker/BugTracker v1.0/BugTracker v1.0/Forms/LoginForm.cs
@@ -4,17 +4,56 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace BugTracker_v1._0
+namespace BugTracker_v1._0.Forms
 {
     public partial class LoginForm : Form
     {
+        private TcpClient client = new TcpClient();
+        private IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 3000);
+        
+
         public LoginForm()
         {
             InitializeComponent();
+            client.Connect(serverEndPoint);
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            NetworkStream clientStream = client.GetStream();
+
+            ASCIIEncoding encoder = new ASCIIEncoding();
+            byte[] buffer = encoder.GetBytes("hello");
+
+            clientStream.Write(buffer, 0, buffer.Length);
+            clientStream.Flush();
+
+            // Receive the TcpServer.response.
+
+            // Buffer to store the response bytes.
+            Byte[] data = new Byte[256];
+
+            // String to store the response ASCII representation.
+            String responseData = String.Empty;
+
+            // Read the first batch of the TcpServer response bytes.
+            Int32 bytes = clientStream.Read(data, 0, data.Length);
+            responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+            if (responseData.Equals("hello"))
+            {
+                
+            }
         }
     }
 }
