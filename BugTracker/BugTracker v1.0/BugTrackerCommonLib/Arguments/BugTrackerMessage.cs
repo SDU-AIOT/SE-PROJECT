dfa39b106611a8f8641e65eaa2e45a7627aa2cf8
adil_ergaliev@mail.ru <adil_ergaliev@mail.ru>
2015-05-13 12:20:28 +0700

    Hello World

--- BugTracker/BugTracker v1.0/BugTrackerCommonLib/Arguments/BugTrackerMessage.cs
+++ BugTracker/BugTracker v1.0/BugTrackerCommonLib/Arguments/BugTrackerMessage.cs
@@ -11,10 +11,26 @@ namespace BugTrackerCommonLib
     [Serializable]
     public class BugTrackerMessage
     {
+        public static int NEW_ISSUE = 1, NEW_USER = 2, NEW_PROJECT = 3;
+        private int messageType;
         /// <summary>
         /// Message text.
         /// </summary>
         public string MessageText { get; set; }
+
+        /// <summary>
+        /// Message type.
+        /// </summary>
+        public int MessageType { 
+            get
+            {
+                return messageType;
+            }
+            set 
+            {
+                messageType = value;
+            }
+        }
 
         /// <summary>
         /// Creates a new BugTrackerMessage.
